7bd74e8659152928f4a751c24bdfc08871d0471b
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2020-07-01 11:23:55 -0300

    one more try to fix images on heroku

--- server.py
+++ server.py
@@ -3,7 +3,7 @@ from dash import Dash
 import dash_bootstrap_components as dbc
 
 server = Flask('multicampidash')
-app = Dash(
+app = Dash(__name__,
     external_stylesheets=[dbc.themes.BOOTSTRAP],
     suppress_callback_exceptions=True,
     meta_tags=[
