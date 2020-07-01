1280022abdc93126f85d17c395f22458eb2af9c7
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2020-07-01 10:32:36 -0300

    one more try to fix images on heroku

--- maps.py
+++ maps.py
@@ -30,10 +30,10 @@ def get_layout():
         ], className='row'),
         html.Div([
             html.Div([
-                html.Img(src=app.get_asset_url('31-05-2020.jpg'), width=1100),
+                html.Img(src='/assets/31-05-2020.jpg', width=1100),
                 html.P('Distribuição Espacial de casos de Covid-19 na V Região - Maio de 2020'),
                 html.Hr(),
-                html.Img(src=app.get_asset_url('30-06-2020.jpg'), width=1100),
+                html.Img(src='/assets/30-06-2020.jpg', width=1100),
                 html.P('Distribuição Espacial de casos de Covid-19 na V Região - Junho de 2020'),
             ], className='col-12 text-center'),
         ], className='row'),
