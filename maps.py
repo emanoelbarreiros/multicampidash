55b45097bdd8bb4360296d70f10ebc1a9c8df055
Emanoel Barreiros <emanoelbarreiros@gmail.com>
2020-07-07 11:10:53 -0300

    new map for july

--- maps.py
+++ maps.py
@@ -30,11 +30,14 @@ def get_layout():
         ], className='row'),
         html.Div([
             html.Div([
-                html.Img(src='/assets/31-05-2020.jpg', width=1100),
-                html.P('Distribuição Espacial de casos de Covid-19 na V Região - Maio de 2020'),
+                html.Img(src='/assets/05-07-2020.jpg', width=1100),
+                html.P('Distribuição Espacial de casos de Covid-19 na V Região - Julho de 2020'),
                 html.Hr(),
                 html.Img(src='/assets/30-06-2020.jpg', width=1100),
                 html.P('Distribuição Espacial de casos de Covid-19 na V Região - Junho de 2020'),
+                html.Hr(),
+                html.Img(src='/assets/31-05-2020.jpg', width=1100),
+                html.P('Distribuição Espacial de casos de Covid-19 na V Região - Maio de 2020'),
             ], className='col-12 text-center'),
         ], className='row'),
     ])
